27bf0ad5ee8ddcb806c252d1729c530dbacac2a9
Vrzadek <>
2023-04-19 18:33:20 +0000

    add base structure for js

---
+++ assets/js/javascript.js
@@ -0,0 +1,12 @@
+const questions = [
+    {text: 'Last world cup winner', options: ['Arg', 'It', 'Fr', 'Br'], answer: 'Arg'},
+    {text: 'Fromula 1 cup winner', options: ['F', 'M', 'B', 'A'], answer: 'M'},
+];
+
+
+function displayQuestion(question) {
+    
+    console.info('should display question');
+}
+
+displayQuestion(' ')
