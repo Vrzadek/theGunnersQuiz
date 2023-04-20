dcecd6d1a1d38f552b5f049a1cd6b154393335a3
Vrzadek <>
2023-04-20 15:49:29 +0000

    adding js to create the questions in order and with correct output

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,3 +3,83 @@ const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull'); 
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+   {
+        question: 'Which of these players is the top-scorer of all time?',
+        choice1: 'Dennis Bergkamp',
+        choice2: 'Ian Wright',
+        choice3: 'Robin Van Persie',
+        choice4: 'Thierry Henry',
+        answer: 4,
+   },
+   {
+        question: 'Who has most appearances for Arsenal?',
+        choice1: 'Lee Dixon',
+        choice2: 'David Seaman',
+        choice3: 'David O Leary',
+        choice4: 'George Armstrong',
+        answer: 3,
+   },
+   {
+        question: 'Who was the player known for his "red hair"?',
+        choice1: 'Robert Pires',
+        choice2: 'Freddie Ljungberg',
+        choice3: 'Emmanuel Petit',
+        choice4: 'Gaël Clichy',
+        answer: 2,
+   },
+   {
+        question: 'Where did Arsenal play before Emirates?',
+        choice1: 'Flybury',
+        choice2: 'Conbury',
+        choice3: 'Lighbury',
+        choice4: 'Highbury',
+        answer: 4,
+   },
+   {
+        question: 'Youngest player to ever play a game for Arsenal?',
+        choice1: 'Ethan Nwaneri',
+        choice2: 'Jack Wilshere',
+        choice3: 'Cesc Fàbregas',
+        choice4: 'Bukayo Saka',
+        answer: 1,
+   }
+]
+
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 5
+
+startGame = () => {
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question 
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        
+    })
+}
